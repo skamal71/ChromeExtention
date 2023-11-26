2c31e1add3d28ae12a7fba89cd7f031f36d35d76
skamal71 <sazidkamal71@gmail.com>
2023-11-25 21:58:16 -0500

    finished the extension

--- index.js
+++ index.js
@@ -1,23 +1,63 @@
 let myLeads = []
+
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
-inputBtn.addEventListener("click", function(){
-  myLeads.push(inputEl.value)
-  renderValue()
-  inputEl.value = ""
+
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorgae = JSON.parse(localStorage.getItem("myLeads"))
+
+const tabBtn = document.getElementById("tab-btn")
+
+
+if (leadsFromLocalStorgae){
+  myLeads = leadsFromLocalStorgae
+  render(myLeads)
+}
+
+tabBtn.addEventListener("click", function(){
+  //gtab the url
+  // chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+  
+  // })
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+  })
+  //console.log(tabs[0].url)
+
 })
 
-function renderValue(){
+
+function render(leads){
   let listItem = ""
-  for (let i = 0; i<myLeads.length; i++){
+  for (let i = 0; i<leads.length; i++){
     listItem += 
     `<li>
-      <a target='_blank' href='${myLeads[i]}'> 
-        ${myLeads[i]}
+      <a target='_blank' href='${leads[i]}'> 
+        ${leads[i]}
       </a>
     </li>`
   }
   ulEl.innerHTML = listItem
 }
+
+deleteBtn.addEventListener("dblclick", function(){
+  localStorage.clear()
+  myLeads = []
+  render(myLeads)
+
+})
+
+
+inputBtn.addEventListener("click", function(){
+  myLeads.push(inputEl.value)
+  inputEl.value = ""
+  //store in local storage
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+  render(myLeads)
+  
+})
+
